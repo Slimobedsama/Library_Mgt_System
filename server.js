eb2d92e6eded06a931facb23c9b9585e58566edc
Slimobedsama <ohwo.okoro@yahoo.com>
2023-05-31 00:20:19 +0100

    deleted env file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const db = require('./config/db');
 
-db();
-const PORT = process.env.PORT;
+// db();
+const PORT = process.env.PORT  || 8000;
 
 app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));

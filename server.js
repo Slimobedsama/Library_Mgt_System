62aa3b42a76b06ef34a6018fd4fc24b209c21113
Slimobedsama <ohwo.okoro@yahoo.com>
2023-05-31 00:02:44 +0100

    mongodb url added to the env file and database connection was setup

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const db = require('./config/db');
 
+db();
 const PORT = process.env.PORT;
 
 app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));

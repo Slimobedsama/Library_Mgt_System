21624bf1eb07cea93b70bbd8d35f9adfc47e742d
Slimobedsama <ohwo.okoro@yahoo.com>
2023-05-31 23:44:41 +0100

    made a change to the router signup

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const Router = express.Router();
 const adminController = require('../controller/adminController');
 
-Router.post('/is-admin/signup', adminController.register);
+Router.post('/is-admin-signup', adminController.register);
 
 module.exports = Router;

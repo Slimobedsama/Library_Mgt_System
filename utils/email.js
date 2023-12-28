0f6d7bf07a0b755d6f8f83e0941d1090ed867c0e
Slimobedsama <ohwo.okoro@yahoo.com>
2023-12-28 18:11:05 +0100

    creation of email file and setup

---
+++ utils/email.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+
+const emailSender = async(info)=> {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: true,
+            auth: {
+                user: process.env.USER_EMAIL,
+                pass: process.env.EMAIL_PASS,
+            }
+        });
+        const userInfo = await transporter.sendMail(info);
+        console.log(`Email sent ${userInfo.messageId}`);
+    } catch (err) {
+        console.log(err.message)
+    }
+}

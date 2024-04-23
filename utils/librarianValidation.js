5cfbb28d781842d5b634a2cffd0652eaadbfa69a
Slimobedsama <ohwo.okoro@yahoo.com>
2024-04-23 01:26:07 +0100

    addition of librarian update validation

--- utils/librarianValidation.js
+++ utils/librarianValidation.js
@@ -22,4 +22,21 @@ const librarianSingupVal =
     }
 ];
 
+const librarianUpdateVal = 
+[
+    body('lastName').notEmpty().withMessage('Enter Last Name'),
+    body('firstName').notEmpty().withMessage('Enter First Name'),
+    body('phone').isMobilePhone('en-NG').withMessage('Enter a valid Nigerian mobile number'),
+    body('password').isStrongPassword({ minLength: 6, minSymbols: 0 }).withMessage('Password Must Be A Minimum Of 6 Characters, 1 Uppercase, 1 Lowercase & 1 Number'),
+    (req, res, next)=> {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.array().map( error => error.msg));
+            return res.status(400).json({ errors: errors.array().map( error => error.msg) });
+        }
+        return next();
+    }
+];
+
 module.exports = librarianSingupVal;
+module.exports = librarianUpdateVal;

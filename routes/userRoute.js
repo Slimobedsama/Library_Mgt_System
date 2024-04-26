f547198016c63d9659af644936961f15c39ce29d
Slimobedsama <ohwo.okoro@yahoo.com>
2024-04-26 08:43:53 +0100

    addition edit user route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,5 +7,6 @@ const { librarianAuth } = require('../middleware/auth');
 Router.get('/', librarianAuth, userController.all);
 Router.get('/:id', librarianAuth, userController.getOne);
 Router.post('/signup', librarianAuth, userSignupVal, userController.register);
+Router.patch('/:id', librarianAuth, userController.modify);
 
 module.exports = Router;

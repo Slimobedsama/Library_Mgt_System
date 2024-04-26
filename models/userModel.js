93aad431b6a423fe9bda7297c2b60691d156dbc8
Slimobedsama <ohwo.okoro@yahoo.com>
2024-04-26 01:35:24 +0100

    modified the schema phoneNumber to string data type and addition of phoneNumber to the signup validation

--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     phoneNumber: {
-        type: Number,
+        type: String,
         required: true,
         unique: true
     },
--- utils/userValidation.js
+++ utils/userValidation.js
@@ -4,8 +4,12 @@ const userSignupVal =
 [
     body('lastName').notEmpty().withMessage('Last name is required'),
     body('firstName').notEmpty().withMessage('First name is required'),
-    body('address').notEmpty().withMessage('Address is required'),
     body('sex').notEmpty().withMessage('Sex is required'),
+    body('address').notEmpty().withMessage('Address is required'),
+    body('phoneNumber').isMobilePhone('en-NG').withMessage('Enter a valid Nigerian mobile number').custom(async value=> {
+        const checkMobile = await User.findOne({ phoneNumber: value });
+        if (checkMobile) throw new Error ('Phone number has already been registered')
+    }),
     body('email').isEmail().withMessage('Enter a valid email').custom(async value=> {
         const checkEmail = await User.findOne({ email: value });
         if (checkEmail) throw new Error ('Email has already been registered')

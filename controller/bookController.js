5d2066e774310389c4e94831d573ae37f655de6c
Slimobedsama <ohwo.okoro@yahoo.com>
2024-04-29 17:44:35 +0100

    create book controller and route added

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,10 +2,21 @@ const Book = require('../models/bookModel');
 
 exports.getAll = async(req, res, next)=> {
     try {
-        const allBooks = await Book.find({ title: 'asc' });
+        const allBooks = await Book.find().sort({ title: 'asc' });
         return res.status(200).json({ message: 'Success', data: allBooks });
     } catch (err) {
         console.log(err.message);
         res.status(500).json({ error: err.message });
+    }
+}
+
+exports.create = async(req, res, next)=> {
+    const { author, title, publishedYear, quantity, ISBN, isAvailable } = req.body;
+    try {
+        const newBook = await Book.create(req.body);
+        return res.status(201).json({ message: 'Success', data: newBook });
+    } catch (err) {
+        console.log(err.message);
+        res.status(400).json({ error: err.message });
     }
 }
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,6 +3,6 @@ const Router = express.Router();
 const bookController = require('../controller/bookController');
 
 Router.get('/', bookController.getAll);
-
+Router.post('/create', bookController.create);
 
 module.exports = Router;

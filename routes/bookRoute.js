66182949d00f4e6867fddf583b01b019f7c370c6
Slimobedsama <ohwo.okoro@yahoo.com>
2024-05-01 17:17:06 +0100

    implementation of librarian authorization on create book and delete book route

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -2,10 +2,11 @@ const express = require('express');
 const Router = express.Router();
 const bookController = require('../controller/bookController');
 const validateCreateBook = require('../utils/bookValidation');
+const { librarianAuth } = require('../middleware/auth');
 
 Router.get('/', bookController.getAll);
 Router.get('/:id', bookController.getOne);
-Router.post('/create', validateCreateBook, bookController.create);
-Router.delete('/:id', bookController.remove);
+Router.post('/create', librarianAuth, validateCreateBook, bookController.create);
+Router.delete('/:id', librarianAuth, bookController.remove);
 
 module.exports = Router;

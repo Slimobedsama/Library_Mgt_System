9a82c2b11911862fdb4f094ea913c6ccd4303f57
Slimobedsama <ohwo.okoro@yahoo.com>
2024-05-01 17:10:15 +0100

    resolved error in circular dependency

--- utils/bookValidation.js
+++ utils/bookValidation.js
@@ -1,6 +1,5 @@
 const { body, validationResult } = require('express-validator');
 const Book = require('../models/bookModel');
-const { options } = require('../routes/bookRoute');
 
 const validateCreateBook = 
 [

0bd60ffed0158878b014ab5b95531ec14cd893c0
Slimobedsama <ohwo.okoro@yahoo.com>
2024-05-01 01:28:30 +0100

    implementation of book validation in book route

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const Router = express.Router();
 const bookController = require('../controller/bookController');
+const validateCreateBook = require('../utils/bookValidation');
 
 Router.get('/', bookController.getAll);
 Router.get('/:id', bookController.getOne);
-Router.post('/create', bookController.create);
+Router.post('/create', validateCreateBook, bookController.create);
 Router.delete('/:id', bookController.remove);
 
 module.exports = Router;

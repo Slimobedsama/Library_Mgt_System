0256ef85d132a1ffe97d1bc14eca916b2fb18554
Slimobedsama <ohwo.okoro@yahoo.com>
2024-05-07 00:40:48 +0100

    create order controller added. adjustment made to the populate method

--- controller/orderController.js
+++ controller/orderController.js
@@ -2,11 +2,24 @@ const Order = require('../models/orderModel');
 
 exports.allOrder = async(req, res, next)=> {
     try {
-        const getAllOrders = await Order.find().populate().sort({ lastName: 'asc' });
+        const getAllOrders = await Order.find().populate('book')
+        .populate('user');
         return res.status(200).json({ message: 'Success', data: getAllOrders });
     } catch (err) {
         console.log(err.message);
         res.status(500).json({ error: err.message });
+    }
+    next();
+}
+
+exports.createOrder = async(req, res, next)=> {
+    let { book, user } = req.body;
+    try {
+        const newOrder = await Order.create(req.body);
+        res.status(201).json({ message: 'Success', data: newOrder });
+    } catch (err) {
+        console.log(err.message);
+        res.status(400).json({ error: err.message });
     }
     next();
 }

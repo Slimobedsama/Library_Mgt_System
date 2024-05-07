840a2047dedeade15ea061c5afd16e2f1fe8d259
Slimobedsama <ohwo.okoro@yahoo.com>
2024-05-07 23:09:55 +0100

    add conditions for quantity and availability of book before and after creating order

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,3 +1,4 @@
+const Book = require('../models/bookModel');
 const Order = require('../models/orderModel');
 
 exports.allOrder = async(req, res, next)=> {
@@ -15,7 +16,24 @@ exports.allOrder = async(req, res, next)=> {
 exports.createOrder = async(req, res, next)=> {
     let { book, user } = req.body;
     try {
+        // CHECK FOR BOOK ID FROM THE BOOK DOCUMENT
+        let findBookId = await Book.findById(book);
+        // CHECKS FOR AVAILABLITY OF BOOK BEFORE CREATING AN ORDER
+        if(findBookId.quantity < 1) {
+            throw new Error('Out of stock');
+        }
+        // CREATE A NEW ORDER
         const newOrder = await Order.create(req.body);
+        // CHECKS FOR QUANTITY OF BOOKS AND AVAILABILITY
+        if(findBookId) {
+            // DECREASE QUANTITY BY 1 WHEN ORDER IS CREATED
+            findBookId.quantity -= 1;
+            if(findBookId.quantity === 0) {
+                // SET AVAILABILITY TO FALSE
+                findBookId.isAvailable = false;
+            }
+        }
+        await findBookId.save();
         res.status(201).json({ message: 'Success', data: newOrder });
     } catch (err) {
         console.log(err.message);

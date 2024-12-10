d6bb7cfd85514271d140a68218627b616c8b03ba
Slimobedsama <ohwo.okoro@yahoo.com>
2024-12-10 18:22:16 +0100

    view engine and static files middleware setup

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv'
 dotenv.config();
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import adminRouter from './routes/adminRoute.js';
@@ -10,11 +12,17 @@ import userRouter from './routes/userRoute.js';
 import bookRouter from './routes/bookRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // JSON PARSER MIDDLEWARE
 app.use(express.json());
 // ENCODED URL PARSER MIDDLEWARE
 app.use(express.urlencoded({extended: true}));
+// STATIC MIDDLEWARE
+app.use(express.static(join(__dirname, 'public')));
 // TEMPLATE ENGINE
+app.set('views', join(__dirname, './view'));
 app.set('view engine', 'ejs');
 // COOKIE MIDDLEWARE
 app.use(cookieParser());

82c2bb28e567c12afa23f280373cef2c075532d1
Obedsama <ohwo.okoro@yahoo.com>
2025-12-05 11:33:36 +0100

    capitalization of first input for first name and last name inputs

--- src/public/javascript/admin/modal.js
+++ src/public/javascript/admin/modal.js
@@ -2,6 +2,9 @@ const modalBtn = document.getElementById('add-librarian');
 const openModalBtn = document.getElementById('open-modal');
 const closeModalBtn = document.getElementById('close-modal');
 const errorOpenModalBtn = document.querySelector('.modal-submit');
+const firstName = document.querySelector('[name="firstName"]');
+const lastName = document.querySelector('[name="lastName"]');
+const email = document.querySelector('[name="email"]');
 
 
 openModalBtn.addEventListener('click', ()=> modalBtn.classList.remove('hidden'));
@@ -20,5 +23,19 @@ document.addEventListener("DOMContentLoaded", () => {
     if (errors.length > 0) {
         // open modal
         document.getElementById("add-librarian").classList.remove("hidden");
+    }
+});
+
+lastName.addEventListener('input', (e)=> {
+    const value = e.target.value;
+    if(value.length > 0) {
+        e.target.value = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+    }
+});
+
+firstName.addEventListener('input', (e)=> {
+    const value = e.target.value;
+    if(value.length > 0) {
+        e.target.value = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
     }
 });
